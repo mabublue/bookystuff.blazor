ae169cc718f1a2463035700d6ee80a2d3ad3949f
Martin Burcombe <martin@burcombe.net>
2018-11-03 22:12:12 +1100

    fix some terraform

--- bookystuff.Terraform/main.tf
+++ bookystuff.Terraform/main.tf
@@ -60,10 +60,6 @@ resource "aws_route" "internet_access" {
   route_table_id         = "${aws_vpc.main.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.gw.id}"
-
-  tags {
-    Name = "bookystuff"
-  }
 }
 
 # Create a NAT gateway with an EIP for each private subnet to get internet connectivity
