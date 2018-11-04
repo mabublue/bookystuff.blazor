a0f80060faf410b60f00aa5c63db0386593354b0
Martin Burcombe <martin@burcombe.net>
2018-11-04 22:44:18 +1100

    tcp protocols

--- bookystuff.Terraform/main.tf
+++ bookystuff.Terraform/main.tf
@@ -117,14 +117,14 @@ resource "aws_security_group" "lb" {
   vpc_id      = "${aws_vpc.main.id}"
 
   ingress {
-    protocol    = "http"
+    protocol    = "tcp"
     from_port   = 80
     to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    protocol    = "https"
+    protocol    = "tcp"
     from_port   = 443
     to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
